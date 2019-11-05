973b2b1666839267d9fd89e19f43b5ba61f9b791
mccallumcarson <mccallumcarson@gmail.com>
2019-11-05 14:17:18 -0800

    Set Material CDNs to 3.2
    Fix glitch occurred by recent version of material by reverting to previous stable version of Material.

--- sw.js
+++ sw.js
@@ -1,4 +1,27 @@
-/** For PWA - An empty service worker! */
-self.addEventListener('fetch', function(event) {
-  /** An empty fetch handler! */
+var cacheName = 'controller-portal-app-caches';
+var filesToCache = [
+  '/index.html',
+  '/css/stylesheet.css'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
